ef41e2f30d301226194f8556a18073f77850c5bb
Single Door Devs <singledoortechnosolutions@gmail.com>
2022-05-02 00:35:45 +0000

    updated to 1.0.0

---
+++ Sources/CustomerAllianceQuestionnaire.xcframework/ios-arm64/CustomerAllianceQuestionnaire.framework/Headers/CustomerAllianceFeedback.h
@@ -0,0 +1,18 @@
+//
+//  CustomerAllianceFeedback.h
+//  CustomerAllianceFeedback
+//
+//  Created by Rahul Jain on 01/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomerAllianceFeedback.
+FOUNDATION_EXPORT double CustomerAllianceFeedbackVersionNumber;
+
+//! Project version string for CustomerAllianceFeedback.
+FOUNDATION_EXPORT const unsigned char CustomerAllianceFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomerAllianceFeedback/PublicHeader.h>
+
+
